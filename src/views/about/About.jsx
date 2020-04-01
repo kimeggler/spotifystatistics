dd21dbdbbd07aa5582970d393c253b0ccee328c3
Kim Eggler <kim.eggler@gmail.com>
2020-04-01 21:05:49 +0200

    changed description in about

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -5,7 +5,7 @@ function About() {
   return (
     <div className='about'>
       <h1>About Statify</h1>
-      <p>Statify is a webapplication based on React. It was developed as part of a schoolproject and is part of a non-profit project.</p>
+      <p>Statify is a webapplication based on React. It was developed as part of a non-profit schoolproject.</p>
       <p className='disclaimer'>*This application has not been approved by Spotify and is selfsustained.</p>
 
       <a href='/'>Back to home</a>
