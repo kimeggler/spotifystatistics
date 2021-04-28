036733df10ec8dc890cc70cd816a14debccf4c84
Kim <kim.eggler@gmail.com>
2021-04-28 14:26:09 +0200

    feat(analyzation): add stats

--- src/views/common/playlist/Playlist.jsx
+++ src/views/common/playlist/Playlist.jsx
@@ -7,69 +7,69 @@ import './style.css';
 
 function Playlist(playlist, activePlaylist, changePlaylist, analyse, closePlaylist) {
   let background = {};
-  let chartoptions = {
-    tooltip: {
-      theme: 'dark',
-    },
-    chart: {
-      height: 200,
-      toolbar: {
-        show: false,
-      },
-    },
-    noData: {
-      text: 'Loading...',
-      align: 'center',
-      verticalAlign: 'middle',
-    },
-    stroke: {
-      curve: 'smooth',
-    },
-    plotOptions: {
-      radar: {
-        size: 150,
-        offsetX: 0,
-        offsetY: 0,
-        polygons: {
-          strokeColors: '#FFFFFF',
-          strokeWidth: 2,
-          connectorColors: '#FFFFFF',
-          fill: {
-            colors: ['#FFFFFF00'],
-          },
-        },
-      },
-    },
-    yaxis: {
-      show: false,
-      labels: {
-        rotate: -90,
-        rotateAlways: true,
-        style: {
-          fontSize: '14px',
-          colors: '#FFFFFF',
-        },
-      },
-    },
-    xaxis: {
-      labels: {
-        show: true,
-        style: {
-          fontSize: '14px',
-          colors: ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
-        },
-      },
-      categories: [
-        'Acousticness',
-        'Danceability',
-        'Energy',
-        'Instrumentalness',
-        'Liveness',
-        'Speechiness',
-        'Happiness',
-      ],
-    },
-  };
+  // let chartoptions = {
+  //   tooltip: {
+  //     theme: 'dark',
+  //   },
+  //   chart: {
+  //     height: 200,
+  //     toolbar: {
+  //       show: false,
+  //     },
+  //   },
+  //   noData: {
+  //     text: 'Loading...',
+  //     align: 'center',
+  //     verticalAlign: 'middle',
+  //   },
+  //   stroke: {
+  //     curve: 'smooth',
+  //   },
+  //   plotOptions: {
+  //     radar: {
+  //       size: 150,
+  //       offsetX: 0,
+  //       offsetY: 0,
+  //       polygons: {
+  //         strokeColors: '#FFFFFF',
+  //         strokeWidth: 2,
+  //         connectorColors: '#FFFFFF',
+  //         fill: {
+  //           colors: ['#FFFFFF00'],
+  //         },
+  //       },
+  //     },
+  //   },
+  //   yaxis: {
+  //     show: false,
+  //     labels: {
+  //       rotate: -90,
+  //       rotateAlways: true,
+  //       style: {
+  //         fontSize: '14px',
+  //         colors: '#FFFFFF',
+  //       },
+  //     },
+  //   },
+  //   xaxis: {
+  //     labels: {
+  //       show: true,
+  //       style: {
+  //         fontSize: '14px',
+  //         colors: ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF'],
+  //       },
+  //     },
+  //     categories: [
+  //       'Acousticness',
+  //       'Danceability',
+  //       'Energy',
+  //       'Instrumentalness',
+  //       'Liveness',
+  //       'Speechiness',
+  //       'Happiness',
+  //     ],
+  //   },
+  // };
 
   if (playlist.images[0]) {
     background = {
@@ -80,7 +80,7 @@ function Playlist(playlist, activePlaylist, changePlaylist, analyse, closePlayli
   const returnSeries = () => {
     console.log(analyse);
     if (analyse.length === 0) {
-      return [];
+      return null;
     }
     return [
       {
@@ -98,14 +98,34 @@ function Playlist(playlist, activePlaylist, changePlaylist, analyse, closePlayli
             <p className="analyse-title">About your Playlist</p>
             <p className="analyse-name">{name}</p>
           </div>
-          <p>
+          {/* <p>
             The content of the playlist is analyzed by Spotify based on a couple of categories. The
             assessment includes the calculation of the proportion of vocal and instrumental parts of
             songs. Furthermore the average energy of each song is being calculated. Additionally
             Spotify gives insights into how euphoric or dystrophic the songs in your playlist are.
-          </p>
+          </p> */}
         </div>
-        <div className="analyse-chart">
+        <div className="chart-area">
+          {analyse.length > 0 && (
+            <div className="analyse-chart">
+              {analyse?.map(serie => {
+                return (
+                  <div className="analyse-serie" key={serie.name}>
+                    <p>{serie.name}</p>
+                    <div className="analyse-chart-box">
+                      <div className="analyse-chart-background"></div>
+                      <div
+                        style={{ width: `${serie.value}%` }}
+                        className="analyse-chart-foreground"
+                      ></div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+        {/* <div className="analyse-chart">
           {returnSeries().length === 0 && <ComponentSpinner />}
           {returnSeries().length > 0 && (
             <ReactApexChart
@@ -116,7 +136,7 @@ function Playlist(playlist, activePlaylist, changePlaylist, analyse, closePlayli
               type="radar"
             />
           )}
-        </div>
+        </div> */}
       </div>
     );
   };
