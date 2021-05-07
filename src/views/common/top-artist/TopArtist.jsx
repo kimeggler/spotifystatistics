7ebe934ded7d730b883d5c319e690f48c0dfce96
Yannik Zimmermann <yannik.z@me.com>
2021-05-08 00:12:54 +0200

    feat(overview-updates): updated styling of overview and moved timerange filter (#35)

--- src/views/common/top-artist/TopArtist.jsx
+++ src/views/common/top-artist/TopArtist.jsx
@@ -1,57 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import cx from 'classnames';
+import React from 'react';
 
 import './style.css';
 import FormattedNumber from '../formattednumber/FormattedNumber';
 
-import rangeOptions from './range-options';
-import Spinner from '../spinner/Spinner';
-import DefaultErrorMessage from '../errors/DefaultErrorMessage';
-import { fetchMyTopArtist } from '../../../services/spotifyservice';
-import useDataHook from '../../../hooks/useDataHook';
-
-const TopArtist = () => {
-  const [timerange, setTimerange] = useState('medium_term');
-  const [artistRequest, setArtistRequest] = useState(() => () => fetchMyTopArtist(timerange));
-  const { data: topArtist, isLoading, hasError } = useDataHook(artistRequest);
-
-  useEffect(() => {
-    setArtistRequest(() => () => fetchMyTopArtist(timerange));
-  }, [timerange]);
-
-  if (hasError) return <DefaultErrorMessage />;
-  if (!topArtist && isLoading !== false) return <Spinner />;
-
-  const background = {
-    backgroundImage: `url(${topArtist.images[0].url})`,
-  };
-
+const TopArtist = ({background, topArtist}) => {
   return (
     <div className="artist-top">
-      {isLoading && <Spinner className="overlay" />}
       <div className="top-card-information top-card-information-artist">
         <p className="top-card-description">Your favourite artist</p>
         <p className="bold top-card-primary">{topArtist.name}</p>
         <p className="top-card-secondary">
           Followers: <FormattedNumber value={topArtist.followers.total} />
         </p>
-        <div className="time-switch padding-right">
-          {rangeOptions.map((option, idx) => (
-            <div
-              key={idx}
-              onClick={() => !isLoading && setTimerange(option.value)}
-              className={cx(
-                'time-button',
-                option.value === timerange ? 'button-selected' : '',
-                isLoading && 'disabled',
-              )}
-            >
-              {option.label}
-            </div>
-          ))}
-        </div>
       </div>
-      <div style={background} className="top-card-image"></div>
+      <div style={background} className="top-card-image top-card-image-artist"></div>
     </div>
   );
 };
