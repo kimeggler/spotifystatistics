2ecd0fc191d32b28c5353531d61cce3e6e9109a9
kimeggler <kim.eggler@gmail.com>
2021-05-07 23:57:30 +0200

    feat(track-rework2.0): basic rework

--- src/views/common/track/Track.jsx
+++ src/views/common/track/Track.jsx
@@ -5,11 +5,13 @@ function Track(track, index) {
   return (
     <div key={track.id} className="track">
       <div className="highlight-circle">
-        <p className="track-index">{index + 1}</p>
+        <p className="track-index bold">{index + 1}</p>
       </div>
       <img alt={track.name} src={track.album.images[0]?.url} className="card-image" />
       <div className="top-section">
-        <p className="image-description bold track-name">{track.name}</p>
+        <p className="image-description bold track-name">
+          {track.name.length > 35 ? `${track.name.substring(0, 32)}...` : track.name}
+        </p>
         <p className="image-description artist-name">{track.artists[0].name}</p>
       </div>
     </div>
