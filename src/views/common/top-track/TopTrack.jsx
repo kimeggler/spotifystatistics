9577e2dc322f6ed287524f11c84455f466d84da8
kimeggler <kim.eggler@gmail.com>
2021-05-08 01:01:56 +0200

    fix alignments

--- src/views/common/top-track/TopTrack.jsx
+++ src/views/common/top-track/TopTrack.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const TopTrack = ({background, topTrack}) => {
+const TopTrack = ({ background, topTrack }) => {
   return (
     <div className="artist-top">
       <div style={background} className="top-card-image top-card-image-track"></div>
       <div className="top-card-information top-card-information-track">
-        <p className="image-description padding-left top-card-description">Your favourite song</p>
-        <p className="image-description padding-left bold top-card-primary">{topTrack.name}</p>
-        <p className="image-description padding-left top-card-secondary">
+        <p className="image-description top-card-description">Your favourite song</p>
+        <p className="image-description bold top-card-primary">{topTrack.name}</p>
+        <p className="image-description top-card-secondary">
           {topTrack.artists[0].name} - {topTrack.album.name}
         </p>
       </div>
     </div>
   );
-}
+};
 
 export default TopTrack;
