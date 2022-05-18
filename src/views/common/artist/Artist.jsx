5e90ccca2b2f869f6b854390a1b8be7b49daaccd
kimeggler <39339183+kimeggler@users.noreply.github.com>
2022-05-18 20:29:12 +0200

    feat: add genre tags on artists (#65)
    * feat: add genre tags on artists
    
    * feat: new yarn lock

--- src/views/common/artist/Artist.jsx
+++ src/views/common/artist/Artist.jsx
@@ -13,7 +13,19 @@ const Artist = (artist, index) => {
   return (
     <div key={artist.id} className="artist">
       <div className="img-container">
-        <div style={background} className="artist-card-image"></div>
+        <div style={background} className="artist-card-image">
+          <div className="artist-card-genres-background"></div>
+          <div className="artist-card-genres">
+            {artist.genres
+              .slice(0, 2)
+              .sort((f, s) => f.length - s.length)
+              .map(g => (
+                <div key={artist.id + g} className="artist-card-genre-tag">
+                  <p>{g.toUpperCase()}</p>
+                </div>
+              ))}
+          </div>
+        </div>
         <p className="artist-rank">{index + 1}</p>
       </div>
       <p className="artist-card-name bold">{artist.name}</p>
